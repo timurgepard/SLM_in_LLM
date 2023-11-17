df6f35c75fbefed2cac31b5fbb4f5bbd1021bf96
timurgepard <timur-ishuov@rambler.ru>
2023-11-17 03:25:44 +0100

    Update LLM_train.py

--- LLM_train.py
+++ LLM_train.py
@@ -1,75 +1,78 @@
 import torch
 import random
 from nanoFFT import BigramLanguageModel
+import pickle
+
+torch.manual_seed(1337)
 
 batch_size = 64
-time_intervals = 256
+time_intervals = 128
 max_iter = 100000
 eval_interval = 500
 learning_rate = 3e-4
 eval_iters = 30
 
 
-import tiktoken
-enc = tiktoken.get_encoding("gpt2")
-print('vocab size: ', enc.n_vocab)
-
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
+with open('./input/tokens.pkl', 'rb') as f:
+    tokens  = pickle.load(f)
 
-with open('input.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+with open('./input/input.pkl', 'rb') as f:
+    input_tokens  = pickle.load(f)
+
+vocab_size = len(tokens)
+print('vocab token size: ', vocab_size)
+print('input text token size: ', len(input_tokens))
 
 
-print("text length: ", len(text))
+stoi = {ch:i for i,ch in enumerate(tokens)}
+itos = {i:ch for i,ch in enumerate(tokens)}
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
 
 
 
 
-data = torch.tensor(enc.encode(text), dtype=torch.long)
-n = int(0.7*len(data))
-
-train_data = data[:n]
-val_data = data[n:]
+data = torch.tensor(encode(input_tokens), dtype=torch.long)
 
 
 torch.manual_seed(1337)
 
 
-def get_batch(split_type):
-    data = train_data if split_type == "train" else val_data
-    ix = torch.randint(len(data) - time_intervals, (batch_size, ))
-    x = torch.stack([data[i:i+time_intervals] for i in ix])
-    y = torch.stack([data[i+1:i+time_intervals+1] for i in ix])
+def get_batch():
+    #var_time = random.randint(32, time_intervals)
+    var_time = time_intervals
+    ix = torch.randint(len(data) - var_time, (batch_size, ))
+    x = torch.stack([data[i:i+var_time] for i in ix])
+    y = torch.stack([data[i+1:i+var_time+1] for i in ix])
     return x.to(device), y.to(device)
 
 
 def get_random_block():
-    data = train_data
     i = random.randint(0, len(data) - time_intervals)
     block = data[i:i+time_intervals].reshape(1, -1).to(device)
     return block
 
 @torch.no_grad()
 def estimate_loss():
-    out = {}
     LLM.eval()
-    for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = LLM(X, targets=Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
+    losses = torch.zeros(eval_iters)
+    for k in range(eval_iters):
+        X, Y = get_batch()
+        _, _, loss = LLM(X, targets=Y)
+        losses[k] = loss.item()
+    out = losses.mean()
     LLM.train()
     return out
 
 
 
-LLM = BigramLanguageModel(enc.n_vocab, time_intervals, n_embed=210, n_head=30, n_layers=7).to(device)
+LLM = BigramLanguageModel(vocab_size, time_intervals, vocab_embed=140, n_embed=140, n_head=14, n_layers=14, device=device).to(device)
 optimizer = torch.optim.Adam(LLM.parameters(), lr=learning_rate)
 
 pytorch_total_params = sum(p.numel() for p in LLM.parameters())
@@ -77,39 +80,44 @@ print('LLM parameters: ', pytorch_total_params)
 
 
 try:
-    LLM.load_state_dict(torch.load('nanoFFT_model.pt'))
+    LLM.load_state_dict(torch.load('LLM_model.pt'))
     context = get_random_block()
-    print(enc.decode(LLM.generate(context, max_new_tokens=500)[0].tolist()))
+    print(decode(LLM.generate(context, max_new_tokens=250)[0].tolist())[-250:])
+    print("loaded")
 except:
     print("no LLM")
 
 for iter in range(max_iter):
 
+    #step = 0
+
     if iter % eval_interval == 0:
+        #nanoFFT.LEARN_BY_HEART = False if step%2==0 else True
+        #step += 1
         losses = estimate_loss()
         context = get_random_block()
-        text = enc.decode(LLM.generate(context, max_new_tokens=50)[0].tolist())[-50:]
+        text = decode(LLM.generate(context, max_new_tokens=50)[0].tolist())[-50:]
         text = text.replace("\n", " <new line> ")
-        print(f"step {iter}, train loss: {losses['train']:.4f}, val loss: {losses['val']:.4f}, text: {text}")
-        if iter>=2000:
+        print(f"step {iter}, train loss: {losses:.4f}, text: {text}")
+        if iter>=1000:
             try:
-                torch.save(LLM.state_dict(), 'nanoFFT_model.pt')
+                torch.save(LLM.state_dict(), 'LLM_model.pt')
             except:
                 print("problem during saving LLM")
 
         if iter>=10000 and iter%10000==0:
             context = get_random_block()
-            print(enc.decode(context[0].tolist()))
+            print(decode(context[0].tolist()))
             print("###########################################")
             print("###########################################")
-            print(enc.decode(LLM.generate(context, max_new_tokens=500)[0].tolist()))
+            print(decode(LLM.generate(context, max_new_tokens=500)[0].tolist()))
             print("###########################################")
             print("###########################################")
     #sample batch of data
-    xb, yb = get_batch("train")
+    xb, yb = get_batch()
 
     #evaluate the loss
-    logits, loss = LLM(xb, targets=yb)
+    _, _, loss = LLM(xb, targets=yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
@@ -119,7 +127,7 @@ for iter in range(max_iter):
 
 context = get_random_block()
 
-print(enc.decode(context[0].tolist()))
+print(decode(context[0].tolist()))
 
 print("###########################################")
 print("###########################################")
@@ -127,6 +135,6 @@ print("###########################################")
 
 
 
-print(enc.decode(LLM.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(LLM.generate(context, max_new_tokens=500)[0].tolist()))
 
 
